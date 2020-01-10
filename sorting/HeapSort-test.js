ff9c99ceeef14e5773eb24f077d75ed694dc95d9
Anwar Ziani <anwaar.ziani@gmail.com>
2020-01-10 11:34:45 -0800

    Implement HeapSort

--- sorting/HeapSort-test.js
+++ sorting/HeapSort-test.js
@@ -3,6 +3,24 @@ const heapSort = require('./HeapSort');
 
 test('heapSort', t => {
   const A = [5, 1, 0, 2, 4, 3];
-  const sorted = heapSort(A);
-  t.deepEqual(sorted, [0, 1, 2, 3, 4, 5]);
+  heapSort(A);
+  t.deepEqual(A, [0, 1, 2, 3, 4, 5]);
+});
+
+test('heapSort empty array', t => {
+  const A = [];
+  heapSort(A);
+  t.deepEqual(A, []);
+});
+
+test('heapSort already sorted array', t => {
+  const A = [0, 1, 2, 3, 4, 5];
+  heapSort(A);
+  t.deepEqual(A, [0, 1, 2, 3, 4, 5]);
+});
+
+test('heapSort one element array', t => {
+  const A = [-1];
+  heapSort(A);
+  t.deepEqual(A, [-1]);
 });
--- sorting/HeapSort.js
+++ sorting/HeapSort.js
@@ -1,6 +1,33 @@
-
 function heapSort(A) {
+  buildHeap(A);
+  let heapEndIndex = A.length - 1;
+  while (heapEndIndex >= 0) {
+    swap(A, 0, heapEndIndex--);
+    heapify(A, heapEndIndex);
+  }
+}
 
+function buildHeap(A) {
+  for (let i = 1; i < A.length; i++) {
+    heapify(A, i);
+  }
+}
+
+function heapify(A, endIndex) {
+  let i = endIndex;
+  while (i > 0) {
+    const parentIndex = Math.floor(i / 2);
+    if (A[i] > A[parentIndex]) {
+      swap(A, i, parentIndex);
+    }
+    i--;
+  }
+}
+
+function swap(A, i, j) {
+  const tmp = A[i];
+  A[i] = A[j];
+  A[j] = tmp;
 }
 
 
