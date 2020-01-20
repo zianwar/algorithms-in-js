2a8f5870f5439b82863552d5bf233978ee5c64e6
Anwar Ziani <anwaar.ziani@gmail.com>
2020-01-19 20:07:24 -0800

    Update FindBridges algorithm

--- graph/FindBridges.js
+++ graph/FindBridges.js
@@ -2,52 +2,53 @@
   Finds all the bridges on an undirected graph.
 */
 
+// Global or class scoped variables
+let id = 0; // To label each vertex with a unique id number.
+
 function findBridges(edges) {
-  let id = 0; // To label each vertex with a unique id number.
   const adjList = buildGraph(edges);
 
   const ids = new Map(); // Maps a vertex to an id number.
-  const lowLinks = new Map(); // Maps a vertex to it low-link value.
+  const low = new Map(); // Maps a vertex to it low-link value.
   const visited = new Map(); // Maps wether a vertex is visited.
 
-  for (const v of adjList.keys()) {
-    lowLinks.set(v, 0);
-    ids.set(v, 0);
+  // Set initial values of visited to false, low and ids to 0.
+  for (const [v] of adjList) {
     visited.set(v, false);
+    low.set(v, 0);
+    ids.set(v, 0);
   }
 
   const bridges = [];
 
-  function dfs(at, prev) {
-    visited.set(at, true);
-    id += 1;
-    ids.set(at, id);
-    lowLinks.set(at, id); // make the default low-link value of v its id.
-
-    if (adjList.has(at)) {
-      for (const to of adjList.get(at)) {
-        if (to === prev) continue;
-
-        if (!visited.get(to)) {
-          dfs(to, at);
-          lowLinks.set(at, Math.min(lowLinks.get(at), lowLinks.get(to)));
-          if (ids.get(at) < lowLinks.get(to)) {
-            bridges.push([at, to]);
-          }
-        } else {
-          lowLinks.set(at, Math.min(lowLinks.get(at), ids.get(to)));
-        }
-      }
-    }
-  }
-
-  for (const v of adjList.keys()) {
+  for (const [v] of adjList) {
     if (!visited.get(v)) {
-      dfs(v, -1);
+      dfs(adjList, visited, ids, low, v, null, bridges);
     }
   }
 
   return bridges;
+}
+
+function dfs(adjList, visited, ids, low, at, prev, bridges) {
+  visited.set(at, true);
+  id += 1;
+  ids.set(at, id);
+  low.set(at, id); // make the default low-link value of v its id.
+
+  for (const to of adjList.get(at)) {
+    if (to === prev) continue;
+
+    if (!visited.get(to)) {
+      dfs(adjList, visited, ids, low, to, at, bridges);
+      low.set(at, Math.min(low.get(at), low.get(to)));
+      if (ids.get(at) < low.get(to)) {
+        bridges.push([at, to]);
+      }
+    } else {
+      low.set(at, Math.min(low.get(at), ids.get(to)));
+    }
+  }
 }
 
 function buildGraph(edges) {
