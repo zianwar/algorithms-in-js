c5bb1ce82074d54030ce11bcb09faaffafe852cf
Anwar Ziani <anwaar.ziani@gmail.com>
2020-01-20 01:13:47 -0800

    Update

--- graph/TopologicalSort-DFS.js
+++ graph/TopologicalSort-DFS.js
@@ -1,26 +1,36 @@
 /*
-  Topological Sort
-
-  A topological sort of a directed graph is a linear ordering of its vertices such that for every directed edge uv from vertex u to vertex v, u comes before v in the ordering.
-  - A topological ordering is possible if and only if the graph has no directed cycles, that is, if it is a directed acyclic graph (DAG).
-  - Any DAG has at least one topological ordering, and algorithms are known for constructing a topological ordering of any DAG in linear time.
-
-  The usual algorithms for topological sorting have running time linear in the number of nodes plus the number of edges, asymptotically.
+ * Topological Sort
+ *
+ * A topological sort of a directed graph is a linear ordering of its vertices such that
+ * for every directed edge uv from vertex u to vertex v, u comes before v in the ordering.
+ * - A topological ordering is possible if and only if the graph has no directed cycles,
+ *   that is, if it is a directed acyclic graph (DAG).
+ * - Any DAG has at least one topological ordering, and algorithms are known for
+ *   constructing a topological ordering of any DAG in linear time.
+ *
+ * The usual algorithms for topological sorting have running time linear in
+ * the number of nodes plus the number of edges, asymptotically.
 */
 
+// Constants for managing possible states of vertices.
 const VISITED = 1;
 const VISITING = 2;
 
-// Depth-first search algorithm for graph topological sort.
-// Time complexity: O(E+V)
+/**
+ * topologicalSort
+ * Depth-first search algorithm for finding a topological sort in a directed graph.
+ *
+ * @param {Array} edges
+ * @returns {Array} ordering Topological ordering of the graph.
+ */
 function topologicalSort(edges) {
   const adjList = buildGraph(edges);
   const state = new Map();
   const ordering = [];
 
-  for (const v of adjList.keys()) {
+  for (const [v] of adjList) {
     if (dfs(adjList, state, ordering, v)) {
-      // "Graph contains a cylce, topological sort is not possible."
+      // Graph contains a cylce, topological sort is not possible.
       return [];
     }
   }
@@ -29,24 +39,43 @@ function topologicalSort(edges) {
   return ordering;
 }
 
+/**
+ * dfs
+ * Performs a Depth-first-search on the directed graph.
+ *
+ * @param {Map} adjList
+ * @param {Map} state
+ * @param {Array} ordering
+ * @param {Any} v
+ * @returns {Boolean} foundCycle
+ */
 function dfs(adjList, state, ordering, v) {
   if (state.get(v) === VISITED) return false;
   if (state.get(v) === VISITING) return true;
 
   state.set(v, VISITING);
 
-  for (const n of adjList.get(v)) {
-    if (dfs(adjList, state, ordering, n)) return true;
+  for (const [n] of adjList) {
+    if (dfs(adjList, state, ordering, n)) {
+      // We found a cycle, return back to the caller.
+      return true;
+    }
   }
 
   state.set(v, VISITED);
   ordering.push(v);
 
+  // We explored all neighbors of v successfully with no cycle found.
   return false;
 }
 
-// Build the graph adjacency list from the edges array
-// [[u,v], [u,w]] => { u: [v,w], v: [], w: [] }
+/**
+ * buildGraph
+ * Build the directed graph adjacency list from the edges array
+ *
+ * @param {Array} edges
+ * @returns {Map} adjList Adjacency list
+ */
 function buildGraph(edges) {
   let adjList = new Map();
   for (let i = 0; i < edges.length; i++) {
@@ -58,5 +87,6 @@ function buildGraph(edges) {
   }
   return adjList;
 }
+
 
 module.exports = topologicalSort;
